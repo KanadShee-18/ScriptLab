157272d28a181e744a848469b84782ac7d4b05ae
Kanad Shee <codewizardkanad@gmail.com>
2024-11-27 22:35:21 +0530

    Added all files for frontend pages

--- frontend/src/components/blogspot/BlogCard.tsx
+++ frontend/src/components/blogspot/BlogCard.tsx
@@ -4,89 +4,9 @@ import { SlLike } from "react-icons/sl";
 import { BsThreeDots } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { PiTrashSimple } from "react-icons/pi";
-import { blogsOverview, deleteBlog } from "../../hooks";
-import { useEffect } from "react";
 
-// type BlogCardProps = {
-//   authorName: string;
-//   title: string;
-//   content: string;
-//   publishedDate?: string;
-//   id: string;
-//   deleteBtn: boolean;
-// };
 
-// const BlogCard = ({
-//   authorName = "Anonymous",
-//   title,
-//   content,
-//   publishedDate,
-//   id,
-//   deleteBtn = false,
-// }: BlogCardProps) => {
-//   const handleDelete = async (id: string) => {
-//     try {
-//       const confirmDelete = window.confirm(
-//         "Are you sure you want to delete this blog?"
-//       );
-//       if (!confirmDelete) return;
 
-//       const result = await deleteBlog({ id });
-//       if (result.success) {
-//         alert("Blog deleted successfully!");
-//       }
-//     } catch (error) {
-//       console.error("Error deleting blog:", error);
-//       alert("Failed to delete the blog. Please try again.");
-//     }
-//   };
-
-//   return (
-//     <div className="cursor-pointer ">
-//       <Link to={`/blog/blog-insider/${id}`}>
-//         <div className="flex flex-row items-center mt-10 gap-x-3">
-//           <Avatar authorName={authorName} />
-//           <span className="flex items-center text-lg font-semibold gap-x-2 text-slate-500">
-//             {authorName} .<p className="text-sm">{publishedDate}</p>
-//           </span>
-//         </div>
-//         <div className="w-full mt-2 text-xl font-bold text-gray-700 text-wrap">
-//           {title}
-//         </div>
-//         <div className="relative mt-2 text-sm font-semibold tracking-wide text-gray-600 group font-inter">
-//           {content?.slice(0, 200) + "..."}
-//         </div>
-//       </Link>
-//       <div className="flex items-center justify-between mt-3 font-semibold text-gray-500 gap-x-2">
-//         <div className="flex gap-x-2">
-//           <IoMdTime size={24} />
-//           <span className="px-2 py-1 text-sm rounded-lg bg-slate-200">
-//             {`${Math.ceil(content?.length / 100)} minutes`} read
-//           </span>
-//         </div>
-//         <div className="flex items-center mr-5 text-xl gap-x-3 w-fit">
-//           {deleteBtn && (
-//             <div
-//               onClick={() => {
-//                 handleDelete(id);
-//               }}
-//               className="text-red-500 cursor-pointer hover:text-red-700"
-//               title="Delete Blog"
-//             >
-//               <PiTrashSimple className="text-2xl" />
-//             </div>
-//           )}
-//           <SlLike title="Like" />
-//           <CiSaveDown2 />
-//           <BsThreeDots />
-//         </div>
-//       </div>
-//       <div className="w-full h-[1px] bg-slate-200 mt-4" />
-//     </div>
-//   );
-// };
-
-// export default BlogCard;
 
 type BlogCardProps = {
   authorName: string;
@@ -95,7 +15,7 @@ type BlogCardProps = {
   publishedDate?: string;
   id: string;
   deleteBtn: boolean;
-  onDelete?: (id: string) => void; // Add onDelete prop
+  onDelete?: (id: string) => void;
 };
 
 const BlogCard = ({
